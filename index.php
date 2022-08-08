e519ff6df75b42a6529ffc41b0e174471427b54b
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2022-08-09 03:21:53 +0700

    memperbaiki bug index data

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ include('koneksi.php');
 //query
 $result = mysqli_query($con, "SELECT * FROM kata");
 
-$datetime1 = new DateTime();
-
 $data = array();
 
 while ($dataKamus = mysqli_fetch_assoc($result)) {
@@ -16,21 +14,24 @@ while ($dataKamus = mysqli_fetch_assoc($result)) {
 }
 
 $jumlahData = count($data);
-$cari = 'reesrvse';
+$cari = 'abu';
 $status = false;
 $dataKe = 0;
+
+$datetime1 = new DateTime();
 
 for ($i = 0; $i < $jumlahData; $i++) {
     $dataKe++;
     if ($data[$i] == $cari) {
-
+        $index = $dataKe;
         $status = true;
     }
 }
 
+$datetime2 = new DateTime();
+
 if ($status) {
-    echo 'wah ketemu ni ada di data ke ' . $dataKe;
-    $datetime2 = new DateTime();
+    echo 'wah ketemu ni ada di data ke ' . $index;
     $interval = $datetime1->diff($datetime2);
     echo '<br>' . $interval->format('%s.%f detik');
 } else {
